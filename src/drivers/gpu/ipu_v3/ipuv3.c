a5be1417b48e0e2659e0b6091488652d4fbd6607
Deryugin <deryugin.denis@gmail.com>
2017-10-07 15:41:05 +0300

    ipu: Add channel parameter operations

--- src/drivers/gpu/ipu_v3/ipuv3.c
+++ src/drivers/gpu/ipu_v3/ipuv3.c
@@ -7,9 +7,63 @@
 #include <hal/reg.h>
 #include <drivers/common/memory.h>
 #include <drivers/video/fb.h>
+#include <string.h>
 #include <util/log.h>
 
 #include "ipu_regs.h"
+
+struct ipu_ch_param_word {
+	uint32_t data[5];
+	uint32_t res[3];
+};
+
+struct ipu_ch_param {
+	struct ipu_ch_param_word word[2];
+};
+
+#define ipu_ch_param_addr(ch) (((struct ipu_ch_param *) IPU_CPMEM) + (ch))
+
+#define _param_word(base, w) \
+	(((struct ipu_ch_param *)(base))->word[(w)].data)
+
+#define ipu_ch_param_set_field(base, w, bit, size, v) { \
+	int i = (bit) / 32; \
+	int off = (bit) % 32; \
+	_param_word(base, w)[i] |= (v) << off; \
+	if (((bit) + (size) - 1) / 32 > i) { \
+		_param_word(base, w)[i + 1] |= (v) >> (off ? (32 - off) : 0); \
+	} \
+}
+
+#define ipu_ch_param_mod_field(base, w, bit, size, v) { \
+	int i = (bit) / 32; \
+	int off = (bit) % 32; \
+	u32 mask = (1UL << size) - 1; \
+	u32 temp = _param_word(base, w)[i]; \
+	temp &= ~(mask << off); \
+	_param_word(base, w)[i] = temp | (v) << off; \
+	if (((bit) + (size) - 1) / 32 > i) { \
+		temp = _param_word(base, w)[i + 1]; \
+		temp &= ~(mask >> (32 - off)); \
+		_param_word(base, w)[i + 1] = \
+			temp | ((v) >> (off ? (32 - off) : 0)); \
+	} \
+}
+
+#define ipu_ch_param_read_field(base, w, bit, size) ({ \
+	u32 temp2; \
+	int i = (bit) / 32; \
+	int off = (bit) % 32; \
+	u32 mask = (1UL << size) - 1; \
+	u32 temp1 = _param_word(base, w)[i]; \
+	temp1 = mask & (temp1 >> off); \
+	if (((bit)+(size) - 1) / 32 > i) { \
+		temp2 = _param_word(base, w)[i + 1]; \
+		temp2 &= mask >> (off ? (32 - off) : 0); \
+		temp1 |= temp2 << (off ? (32 - off) : 0); \
+	} \
+	temp1; \
+})
 
 /* Default config      Parameters            Possible values */
 static const int ipu_word_size_bits = 24; /* 8, 16, 24 or 32 */
@@ -86,8 +140,42 @@ int ipu_init_sync_panel(int ch) {
 	return 0;
 }
 
+static inline void ipu_ch_params_set_packing(struct ipu_ch_param *p,
+					      int red_width, int red_offset,
+					      int green_width, int green_offset,
+					      int blue_width, int blue_offset,
+					      int alpha_width, int alpha_offset) {
+	/* Setup red width and offset */
+	ipu_ch_param_set_field(p, 1, 116, 3, red_width - 1);
+	ipu_ch_param_set_field(p, 1, 128, 5, red_offset);
+	/* Setup green width and offset */
+	ipu_ch_param_set_field(p, 1, 119, 3, green_width - 1);
+	ipu_ch_param_set_field(p, 1, 133, 5, green_offset);
+	/* Setup blue width and offset */
+	ipu_ch_param_set_field(p, 1, 122, 3, blue_width - 1);
+	ipu_ch_param_set_field(p, 1, 138, 5, blue_offset);
+	/* Setup alpha width and offset */
+	ipu_ch_param_set_field(p, 1, 125, 3, alpha_width - 1);
+	ipu_ch_param_set_field(p, 1, 143, 5, alpha_offset);
+}
+
+int ipu_ch_write_params(struct fb_info *fbi) {
+	struct ipu_ch_param params;
+
+	memset(&params, 0, sizeof(params));
+
+	/* RGB565 */
+	ipu_ch_param_set_field(&params, 0, 107, 3, 3);	/* bits/pixel */
+	ipu_ch_param_set_field(&params, 1, 85, 4, 7);	/* pix format */
+	ipu_ch_param_set_field(&params, 1, 78, 7, 15);	/* burst size */
+
+	ipu_ch_params_set_packing(&params, 5, 0, 6, 5, 5, 11, 8, 16);
+
+	return 0;
+}
+
 int ipu_init_channel_buffer(struct fb_info *fbi) {
-	log_debug("%s is NIY", __func__);
+	ipu_ch_write_params(fbi);
 	return 0;
 }
 
