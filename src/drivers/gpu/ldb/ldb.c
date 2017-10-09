28ad3d24be1f7ebaa9f83ccf83b178972216155a
Deryugin <deryugin.denis@gmail.com>
2017-10-09 14:21:55 +0300

    ldb: Add CCM- and IOMUXC-related stuff here from IPU

--- src/drivers/gpu/ldb/ldb.c
+++ src/drivers/gpu/ldb/ldb.c
@@ -24,9 +24,31 @@ EMBOX_UNIT_INIT(ldb_init);
 #define LDB_CH1_DI0     0x4
 #define LDB_CH1_DI1     0xC
 
+#define IOMUXC_GPR3	0x20E000C
+
+#define CCM_ANALOG_MISC1 0x20C8160
+
+extern int clk_enable(char *clk_name);
+
 static int ldb_init(void) {
-	/* Enable clock? CCM_CCGR3 */
+	/* Enable LVDS1/2 output buffers */
+	REG32_CLEAR(CCM_ANALOG_MISC1, (1 << 12) | (1 << 13));
+	REG32_ORIN(CCM_ANALOG_MISC1, (1 << 10) | (1 << 11));
+
+	clk_enable("ldb_di0");
+	//clk_enable("ldb_di1");
+
 	REG32_STORE(LDB_CTRL, LDB_CH0_DI0 | LDB_CH1_DI0);
+
+	/****************************
+	 * Configure IOMUX for LVDS:
+	 *      IPU1 DI0 -> LVDS1
+	 *      IPU1 DI0 -> LVDS2
+	 ****************************/
+	REG32_CLEAR(IOMUXC_GPR3, 3 << 8);
+	REG32_CLEAR(IOMUXC_GPR3, 3 << 6);
+	REG32_ORIN(IOMUXC_GPR3, 0 << 8);
+	REG32_ORIN(IOMUXC_GPR3, 0 << 6);
 
 	return 0;
 }
@@ -37,3 +59,17 @@ static struct periph_memory_desc ldb_mem = {
 };
 
 PERIPH_MEMORY_DEFINE(ldb_mem);
+
+static struct periph_memory_desc ldb_iomux_mem = {
+	.start = 0x20E0000,
+	.len   = 0x1000,
+};
+
+PERIPH_MEMORY_DEFINE(ldb_iomux_mem);
+
+static struct periph_memory_desc ccm_analog_mem = {
+	.start = 0x20C8000,
+	.len   = 0x200,
+};
+
+PERIPH_MEMORY_DEFINE(ccm_analog_mem);
